a15190d0b6133c70d35b23d72e0251080919265e
alejandrodomingueztorres <85324176+alejandrodomingueztorres@users.noreply.github.com>
2025-04-12 00:27:42 -0500

    Ajustes Player
    Se ajustan el jugador para que no se haga daño con el disparo del arma y se modifica al enemigo "oso" y se ponen varios en el nivel

--- Taller2CGG52GAME2D/Assets/Scripts/BearScript.cs
+++ Taller2CGG52GAME2D/Assets/Scripts/BearScript.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class BearScript : MonoBehaviour
 {
+    [SerializeField] private Transform[] puntosMovimiento;
+    [SerializeField] private float velocidadMovimiento;
+    private int siguientePlataforma = 1;
+    private bool ordenPlataformas = true;
     public GameObject Bullet1;
     public GameObject player;
     private float LastAtack;
@@ -24,7 +28,27 @@ public class BearScript : MonoBehaviour
             Atack();
             LastAtack = Time.time;
         }
-       
+
+        if (ordenPlataformas && siguientePlataforma + 1 >= puntosMovimiento.Length)
+        {
+            ordenPlataformas = false;
+        }
+        if (!ordenPlataformas && siguientePlataforma <= 0)
+        {
+            ordenPlataformas = true;
+        }
+        if (Vector2.Distance(transform.position, puntosMovimiento[siguientePlataforma].position) < 0.1f)
+        {
+            if (ordenPlataformas)
+            {
+                siguientePlataforma += 1;
+            }
+            else
+            {
+                siguientePlataforma -= 1;
+            }
+        }
+        transform.position = Vector2.MoveTowards(transform.position, puntosMovimiento[siguientePlataforma].position, velocidadMovimiento * Time.deltaTime);
     }
     private void Atack()
     {
